24b89acc0582734f51f5527ebe753f54f6c2d746
Jhon <jhonstupak@gmail.com>
2018-10-21 16:41:09 +0300

    Add serachbar, add extensions

---
+++ Translator/Extensions/ObservableExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Translator.Extensions
+{
+    public static class ObservableExtension
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> source)
+        {
+            return new ObservableCollection<T>(source);
+        }
+    }
+}
--- Translator/ViewModels/DetailPageViewModel.cs
+++ Translator/ViewModels/DetailPageViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using Translator.Extensions;
 
 namespace Translator.ViewModels
 {
     public class DetailPageViewModel: ViewModel
     {
+        private List<WordViewModel> allItems;
         private ObservableCollection<WordViewModel> items;
 
         public ObservableCollection<WordViewModel> Items
@@ -20,6 +23,37 @@ namespace Translator.ViewModels
                     this.OnPropertyChanged();
                 }
             }
+        }
+
+        public string SearchText
+        {
+            set
+            {
+                if (value!= null)
+                {
+                    Items = allItems
+                        .Where(item => item.Original.Contains(value))
+                        .ToObservableCollection();
+                }
+            }
+        }
+
+
+        public DetailPageViewModel()
+        {
+
+        }
+
+
+        public void OnAppearing()
+        {
+            allItems = App.WordsRepository
+                .GetAll()
+                .Select(word => word.ToViewModel(this))
+                .OrderBy(word => word.Original)
+                .ToList();
+
+            Items = allItems.ToObservableCollection();
         }
     }
 }
