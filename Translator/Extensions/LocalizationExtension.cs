4e5a929c683840404cdfbb97851dc6fc8df11465
GODELTECH\i.stupak <i.stupak@godeltech.com>
2018-10-29 17:13:59 +0300

    Add ru resource localization

---
+++ Translator/Extensions/LocalizationExtension.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using Translator.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Translator.Extensions
+{
+    [ContentProperty(nameof(Text))]
+    public class LocalizationExtension: IMarkupExtension
+    {
+        private static readonly ResourceManager ResourceManager =new ResourceManager(
+            ConstantService.AppResourcePath,
+            IntrospectionExtensions.GetTypeInfo(typeof(LocalizationExtension)).Assembly);
+
+        private readonly CultureInfo cultureInfo;
+
+        public string Text { get; set; }
+
+
+        public LocalizationExtension()
+        {
+            cultureInfo = new CultureInfo("ru");
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return string.Empty;
+
+            string localization = ResourceManager.GetString(Text, cultureInfo) ?? Text;
+
+            return localization;
+        }
+    }
+}
