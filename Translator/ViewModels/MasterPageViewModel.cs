c4011ae0048a1c9e3ada16fa063dcdbdec593efc
GODELTECH\i.stupak <i.stupak@godeltech.com>
2018-10-30 10:01:25 +0300

    Settings page

---
+++ Translator/ViewModels/MasterPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Translator.ViewModels
+{
+    public class MasterPageViewModel:ViewModel
+    {
+        public INavigation Navigation { get; set; }
+        public ICommand SettingsCommand { get; set; }
+
+
+        public MasterPageViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+
+            SettingsCommand = new Command(ShowSettingsPage);
+        }
+
+
+        private async void ShowSettingsPage()
+        {
+            await Navigation.PushModalAsync(null);
+        }
+    }
+}
